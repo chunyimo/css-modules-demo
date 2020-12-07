6410d3adf02d588de48b5dc05f6254289b4765ae
chunyi.mo <mcyatom@gmail.com>
2020-12-08 00:47:33 +0800

    配置css modules

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import styles from './index.css';
+// @ts-ignore
+import { grayColor, redColor } from './index.css';
 
 const Title: React.FC<any> = (props) => {
   return (
     <div>
-      <h1 className={styles.grayColor}>gray title</h1>
-      <h1 className={styles.redColor}>red color</h1>
+      <h1 className={grayColor}>gray title</h1>
+      <h1 className={redColor}>red color</h1>
     </div>
   );
 };
